b82916dc34f3f16c4585afea43ad63a498470bca
Rodrigo Emanuel <drigosantos@gmail.com>
2021-05-07 09:26:03 -0300

    Routes

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,62 +1,62 @@
 const DataTable = () => {
   return (
     <div className="table-responsive">
-    <table className="table table-striped table-sm">
-      <thead>
-        <tr>
-          <th>Data</th>
-          <th>Vendedor</th>
-          <th>Clientes visitados</th>
-          <th>Negócios fechados</th>
-          <th>Valor</th>
-        </tr>
-      </thead>
-        <tbody>
+      <table className="table table-striped table-sm">
+        <thead>
           <tr>
-            <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
+            <th>Data</th>
+            <th>Vendedor</th>
+            <th>Clientes visitados</th>
+            <th>Negócios fechados</th>
+            <th>Valor</th>
           </tr>
-          <tr>
-            <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
-          </tr>
-          <tr>
-            <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
-          </tr>
-          <tr>
-            <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
-          </tr>
-          <tr>
-            <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
-          </tr>
-          <tr>
-            <td>22/04/2021</td>
-            <td>Barry Allen</td>
-            <td>34</td>
-            <td>25</td>
-            <td>15017.00</td>
-          </tr>
-        </tbody>
-    </table>
-</div>
+        </thead>
+          <tbody>
+            <tr>
+              <td>22/04/2021</td>
+              <td>Barry Allen</td>
+              <td>34</td>
+              <td>25</td>
+              <td>15017.00</td>
+            </tr>
+            <tr>
+              <td>22/04/2021</td>
+              <td>Barry Allen</td>
+              <td>34</td>
+              <td>25</td>
+              <td>15017.00</td>
+            </tr>
+            <tr>
+              <td>22/04/2021</td>
+              <td>Barry Allen</td>
+              <td>34</td>
+              <td>25</td>
+              <td>15017.00</td>
+            </tr>
+            <tr>
+              <td>22/04/2021</td>
+              <td>Barry Allen</td>
+              <td>34</td>
+              <td>25</td>
+              <td>15017.00</td>
+            </tr>
+            <tr>
+              <td>22/04/2021</td>
+              <td>Barry Allen</td>
+              <td>34</td>
+              <td>25</td>
+              <td>15017.00</td>
+            </tr>
+            <tr>
+              <td>22/04/2021</td>
+              <td>Barry Allen</td>
+              <td>34</td>
+              <td>25</td>
+              <td>15017.00</td>
+            </tr>
+          </tbody>
+      </table>
+  </div>
   );
 }
 
